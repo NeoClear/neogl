34b5c0145bc959f69e2a768f01e91bd48b695a4e
luke <saogong123456@outlook.com>
2017-07-06 17:20:11 +0800

    the basic stuff

--- src/font.h
+++ src/font.h
@@ -1,9 +1,9 @@
 #include <GLFW/glfw3.h>
 #include "nl_namespace.h"
 
-extern GLubyte nl_font_i[48];
+extern GLubyte nl_font_i[128][48];
 
 namespace nl_font
 {
-    void draw(int _w, int _h);
+    void draw(int _w, int _h, int m_w, int m_h);
 }
